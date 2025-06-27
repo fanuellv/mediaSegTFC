1e1c7fc272e2a1ab3283a62b1e1c8c96639dbcc1
fanuellv <fanueljuniorlv2@gmail.com>
2025-06-27 20:08:22 +0100

    responsive p3

--- resources/js/components/painel/inicio.tsx
+++ resources/js/components/painel/inicio.tsx
@@ -13,7 +13,6 @@ export default function Inicio() {
             <div className="flex w-full flex-col gap-4 sm:w-3/5">
                 {/* Contratação de Plano */}
                 <div className="rounded-2xl bg-white p-4 sm:h-[60%]">
-                    
                     <SimuladorPlanoForm />
                 </div>
 
@@ -22,10 +21,13 @@ export default function Inicio() {
                     <div className="top-0 z-10 bg-white pb-2 font-bold">
                         <h1 className="text-lg">Top Seguradoras</h1>
                     </div>
-                    <div className="scrollbar-thin scrollbar-thumb-gray-300 flex-1 overflow-y-auto pr-1" style={{
+                    <div
+                        className="scrollbar-thin scrollbar-thumb-gray-300 flex-1 overflow-y-auto pr-1"
+                        style={{
                             scrollbarWidth: 'none', // Firefox
                             msOverflowStyle: 'none', // IE 10+
-                        }}>
+                        }}
+                    >
                         <ListSeguradora />
                     </div>
                 </div>
@@ -34,20 +36,17 @@ export default function Inicio() {
             {/* Coluna Direita */}
             <div className="flex w-full flex-col gap-4 sm:w-2/5">
                 {/* Planos em Alta */}
-                <div className="flex flex-col rounded-2xl bg-white p-4 sm:h-[60%]">
+                <div className="flex h-auto flex-col rounded-2xl bg-white p-4 sm:h-[60%]">
                     <div className="top-0 z-10 bg-white pb-2 font-bold">
                         <h1 className="text-lg">Planos em Alta</h1>
                     </div>
-                    <div className="scrollbar-thin scrollbar-thumb-gray-300 flex-1 overflow-y-auto pr-1" style={{
-                            scrollbarWidth: 'none', // Firefox
-                            msOverflowStyle: 'none', // IE 10+
-                        }}>
+                    <div className="scrollbar-thin scrollbar-thumb-gray-300 flex-1 overflow-y-auto pr-1">
                         <ListaPlanos />
                     </div>
                 </div>
 
                 {/* Comentários */}
-                <div className="relative bottom-15 sm:bottom-0 flex flex-col rounded-2xl bg-white p-4 sm:h-[40%]">
+                <div className="flex h-auto flex-col rounded-2xl bg-white p-4 sm:h-[40%]">
                     <div className="top-0 z-10 flex items-center justify-between bg-white pb-2">
                         <h1 className="text-lg font-bold">Comentários</h1>
                         <button onClick={() => setMostrarModal(true)} className="rounded bg-[#0153A5] px-3 py-1 text-sm text-white hover:bg-blue-600">
@@ -55,10 +54,7 @@ export default function Inicio() {
                         </button>
                         <ComentarioModal isOpen={mostrarModal} onClose={() => setMostrarModal(false)} />
                     </div>
-                    <div className="scrollbar-thin scrollbar-thumb-gray-300 flex-1 overflow-y-auto pr-1" style={{
-                            scrollbarWidth: 'none', // Firefox
-                            msOverflowStyle: 'none', // IE 10+
-                        }}>
+                    <div className="pb-16 sm:pb-0 scrollbar-thin scrollbar-thumb-gray-300 flex-1 overflow-y-auto pr-1">
                         <ComentariosLista />
                     </div>
                 </div>
