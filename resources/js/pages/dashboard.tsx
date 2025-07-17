b66a56ebbf4473b3ce769538e426ab89b22aae17
fanuellv <fanueljuniorlv2@gmail.com>
2025-07-17 13:26:53 +0100

    alt

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,4 +1,4 @@
-import DefaultLayout from '@/Layouts/DefaultLayout';
+import DefaultLayout from '@/Layouts/DefaultLayout.jsx';
 import Loader from '@/components/uiMediaseg/Loader';
 import { useEffect, useState } from 'react';
 
