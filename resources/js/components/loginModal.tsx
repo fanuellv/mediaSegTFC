3eb7c96ad9355f9cde8376c13a74c23ea279c6a7
fanuellv <fanueljuniorlv2@gmail.com>
2025-07-18 23:39:06 +0100

    responsiv login

--- resources/js/components/loginModal.tsx
+++ resources/js/components/loginModal.tsx
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import hero from '/public/img/heroLogin.svg';
 import logoWhite from '/public/img/logoWhite.svg';
 
 import { FaRegUser } from 'react-icons/fa';
 import { TbLockPassword } from 'react-icons/tb';
+import Loader from './loader';
+import { useForm } from '@inertiajs/react';
 
 const LoginModal: React.FC = () => {
+    const { data, setData, post, processing, errors } = useForm({
+        nif: '',
+        senha: '',
+    });
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log('Enviando login com:', data);
+
+        post('/login', {
+            onError: (errors) => {
+                console.log('❌ Erros:', errors);
+            },
+        });
+    };
+
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const timeout = setTimeout(() => setLoading(false), 1500);
+
+        const token = document.querySelector('meta[name="csrf-token"]')?.getAttribute('content');
+        console.log('CSRF Token:', token);
+
+        return () => clearTimeout(timeout);
+    }, []);
+
+    if (loading) return <Loader />;
     return (
         <div className="flex w-full">
             {/* Seção de Conteúdo */}
@@ -14,7 +44,9 @@ const LoginModal: React.FC = () => {
                 <img src={logoWhite} alt="" style={{ width: '200px', height: 'auto', maxWidth: '100%' }} />
                 <img src={hero} alt="" style={{ width: '300px', height: 'auto', maxWidth: '100%' }} />
                 <h1 className="text-2xl font-bold">Bem-Vindo</h1>
-                <p className='text-xs font-light mb-10'>A MediaSeg! Explore nossa coleção selecionada de conteúdo voltado a seguros e descubra uma realidade ainda não explorada.</p>
+                <p className="mb-10 text-xs font-light">
+                    A MediaSeg! Explore nossa coleção selecionada de conteúdo voltado a seguros e descubra uma realidade ainda não explorada.
+                </p>
             </div>
 
             {/* Seção de Login */}
@@ -27,23 +59,37 @@ const LoginModal: React.FC = () => {
                     </a>
                 </p>
 
-                <form className="space-y-4">
+                <form onSubmit={handleSubmit} className="space-y-4">
                     <div className="flex items-center">
-                        <input type="text" className="w-full rounded border-b border-b-gray-300 px-4 py-2" placeholder="Nome do Usuário | NIF" />
+                        <input
+                            type="text"
+                            value={data.nif}
+                            onChange={(e) => setData('nif', e.target.value)}
+                            className="w-full rounded border-b border-b-gray-300 px-4 py-2"
+                            placeholder="Nome do Usuário | NIF"
+                        />
+                        {errors.nif && <div className="text-red-500">{errors.nif}</div>}
                         <span className="absolute right-65 text-gray-400">
                             <FaRegUser />
                         </span>
                     </div>
 
                     <div className="flex items-center">
-                        <input type="password" className="w-full rounded border-b border-b-gray-300 px-4 py-2" placeholder="Palavra-passe" />
+                        <input
+                            type="password"
+                            value={data.senha}
+                            onChange={(e) => setData('senha', e.target.value)}
+                            className="w-full rounded border-b border-b-gray-300 px-4 py-2"
+                            placeholder="Palavra-passe"
+                        />
+                         {errors.senha && <div className="text-red-500">{errors.senha}</div>}
                         <span className="absolute right-65 text-gray-400">
                             <TbLockPassword />
                         </span>
                     </div>
 
-                    <div className="flex justify-between mt-5">
-                        <button type="submit" className=" rounded bg-[#0153A5] px-4 py-2 font-semibold text-white">
+                    <div className="mt-5 flex justify-between">
+                        <button type="submit" disabled={processing} className="rounded bg-[#0153A5] px-4 py-2 font-semibold text-white">
                             Iniciar Sessão
                         </button>
                         <div className="flex items-center space-x-2">
@@ -53,7 +99,9 @@ const LoginModal: React.FC = () => {
                             </label>
                         </div>
                     </div>
-                    <a href="#" className='font-light text-xs text-blue-400 underline'>Esqueci-me da Palavra-passe</a>
+                    <a href="#" className="text-xs font-light text-blue-400 underline">
+                        Esqueci-me da Palavra-passe
+                    </a>
                 </form>
             </div>
         </div>
