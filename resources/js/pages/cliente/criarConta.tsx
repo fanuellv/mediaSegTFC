c64f5d5fdf7a6b5a5dedc1d1551977dab01b3d93
fanuellv <fanueljuniorlv2@gmail.com>
2025-07-18 22:41:24 +0100

    responsiv cont

--- resources/js/pages/cliente/criarConta.tsx
+++ resources/js/pages/cliente/criarConta.tsx
@@ -86,9 +86,9 @@ export default function CriarCliente() {
             <form onSubmit={handleSubmit} encType="multipart/form-data" className="flex min-h-screen w-full flex-col bg-white sm:flex-row">
                 {/* Lado esquerdo (logo + foto) */}
                 <div className="flex flex-col items-center justify-center gap-6 bg-[#0153A5] p-10 text-white sm:w-2/5">
-                    <img  src={logoWhite} alt="Logo" className="w-50 sm:absolute sm:top-10 sm:left-0" />
+                    <img src={logoWhite} alt="Logo" className="absolute sm:top-10 sm:left-0 sm:w-50 w-40 top-5 right-0" />
 
-                    <div className="flex flex-col items-center text-center">
+                    <div className="flex flex-row items-center text-center sm:flex-col gap-2">
                         <div className="h-52 w-52 overflow-hidden rounded-full bg-gray-100">
                             {fotoPreview ? (
                                 <img src={fotoPreview} alt="Preview da foto" className="h-full w-full object-cover" />
@@ -96,33 +96,35 @@ export default function CriarCliente() {
                                 <span className="flex h-full items-center justify-center text-sm text-gray-400">Sem foto</span>
                             )}
                         </div>
-                        <h2 className="mt-4 text-lg font-semibold">Adicionar foto</h2>
-                        <p className="mb-2 text-sm font-light">Utilize o botão abaixo para fazer upload da sua foto</p>
-                        <div>
-                            <input id="foto" name="foto" type="file" accept="image/*" onChange={handleFileChange} className="hidden" />
+                        <div className='text-left sm:text-center'>
+                            <h2 className="mt-4 text-lg font-semibold">Adicionar foto</h2>
+                            <p className="mb-2 sm:text-sm text-xs font-light">Utilize o botão abaixo para fazer upload da sua foto</p>
+                            <div>
+                                <input id="foto" name="foto" type="file" accept="image/*" onChange={handleFileChange} className="hidden" />
 
-                            <label
-                                htmlFor="foto"
-                                className="inline-block cursor-pointer rounded-lg bg-white px-4 py-2 text-sm font-medium text-white transition hover:bg-blue-700"
-                            >
-                                +
-                            </label>
+                                <label
+                                    htmlFor="foto"
+                                    className="inline-block cursor-pointer rounded-full bg-white px-4 py-2 text-2xl font-medium text-[#0153A5] transition hover:bg-blue-400 hover:text-white"
+                                >
+                                    +
+                                </label>
+                            </div>
                         </div>
                     </div>
                 </div>
 
                 {/* Lado direito (formulário) */}
                 <div className="flex flex-col justify-center gap-6 p-10 sm:w-3/5">
-                    <div >
-                        <h1 className="text-2xl font-bold text-[#0153A5]">Registrar-se</h1>
+                    <div className="flex items-center gap-2">
+                        <h1 className="text-3xl font-bold text-[#0153A5]">Registrar-se</h1>
                         <p className="text-sm">
-                            Já tens conta? <span className="cursor-pointer text-blue-600 underline">Iniciar Sessão</span>
+                            Já tens conta? <span className="cursor-pointer text-blue-500 underline">Iniciar Sessão</span>
                         </p>
                     </div>
 
-                    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
-                        <div className='flex gap-2'>
-                            <label htmlFor="nome" className="flex items-center gap-2  font-medium">
+                    <div className="grid grid-cols-2 gap-4 sm:grid-cols-2">
+                        <div className="flex gap-2">
+                            <label htmlFor="nome" className="flex items-center gap-2 font-medium">
                                 <MdDriveFileRenameOutline />
                             </label>
                             <input
@@ -134,7 +136,7 @@ export default function CriarCliente() {
                             />
                         </div>
 
-                        <div className='flex gap-2'>
+                        <div className="flex gap-2">
                             <label htmlFor="sobrenome" className="flex items-center gap-2 text-sm font-medium">
                                 <MdDriveFileRenameOutline />
                             </label>
@@ -147,7 +149,7 @@ export default function CriarCliente() {
                             />
                         </div>
 
-                        <div className='flex gap-2'>
+                        <div className="flex gap-2">
                             <label htmlFor="nome_usuario" className="flex items-center gap-2 text-sm font-medium">
                                 <MdDriveFileRenameOutline />
                             </label>
@@ -160,9 +162,7 @@ export default function CriarCliente() {
                             />
                         </div>
 
-                        
-
-                        <div className='flex gap-2'>
+                        <div className="flex gap-2">
                             <label htmlFor="dataRegistro" className="flex items-center gap-2 text-sm font-medium">
                                 <MdDateRange />
                             </label>
@@ -175,7 +175,7 @@ export default function CriarCliente() {
                             />
                         </div>
 
-                        <div className='flex gap-2'>
+                        <div className="flex gap-2">
                             <label htmlFor="nif" className="flex items-center gap-2 text-sm font-medium">
                                 <FaIdCard />
                             </label>
@@ -188,9 +188,9 @@ export default function CriarCliente() {
                             />
                         </div>
 
-                        <div className='flex gap-2'>
+                        <div className="flex gap-2">
                             <label htmlFor="telefone" className="flex items-center gap-2 text-sm font-medium">
-                                <IoMdPhonePortrait /> 
+                                <IoMdPhonePortrait />
                             </label>
                             <input
                                 name="telefone"
@@ -200,36 +200,34 @@ export default function CriarCliente() {
                                 className="input w-full rounded border p-2 text-sm"
                             />
                         </div>
-
-                        
                     </div>
-                    <div className='flex gap-2'>
-                            <label htmlFor="email" className="flex items-center gap-2 text-sm font-medium">
-                                <MdOutlineMailOutline />
-                            </label>
-                            <input
-                                name="email"
-                                type="email"
-                                value={form.email}
-                                onChange={handleChange}
-                                placeholder="Email"
-                                className="input w-full rounded border p-2 text-sm"
-                            />
-                        </div>
-                    <div className='flex gap-2'>
-                            <label htmlFor="senha" className="flex items-center gap-2 text-sm font-medium">
-                                <MdOutlinePassword />
-                            </label>
-                            <input
-                                name="senha"
-                                type="password"
-                                value={form.senha}
-                                onChange={handleChange}
-                                placeholder="Senha"
-                                className="input w-full rounded border p-2 text-sm"
-                            />
-                        </div>
-                    <button type="submit" className="mt-4 w-full rounded bg-[#0153A5] px-6 py-2 text-white transition hover:bg-[#013f80] sm:w-auto">
+                    <div className="flex gap-2">
+                        <label htmlFor="email" className="flex items-center gap-2 text-sm font-medium">
+                            <MdOutlineMailOutline />
+                        </label>
+                        <input
+                            name="email"
+                            type="email"
+                            value={form.email}
+                            onChange={handleChange}
+                            placeholder="Email"
+                            className="input w-full rounded border p-2 text-sm"
+                        />
+                    </div>
+                    <div className="flex gap-2">
+                        <label htmlFor="senha" className="flex items-center gap-2 text-sm font-medium">
+                            <MdOutlinePassword />
+                        </label>
+                        <input
+                            name="senha"
+                            type="password"
+                            value={form.senha}
+                            onChange={handleChange}
+                            placeholder="Senha"
+                            className="input w-full rounded border p-2 text-sm"
+                        />
+                    </div>
+                    <button type="submit" className="mt-4 rounded bg-[#0153A5] px-4 py-2 font-bold text-white transition hover:bg-blue-600 sm:w-50">
                         Criar Conta
                     </button>
                 </div>
